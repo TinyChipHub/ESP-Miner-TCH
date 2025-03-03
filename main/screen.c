025b458000d07745c6565d78640523c61a082aa9
AaronCai <aaron.cai@xaiten.com>
2025-03-03 21:53:58 +0800

    add long text support for mac and mining address

--- main/screen.c
+++ main/screen.c
@@ -338,9 +338,13 @@ static lv_obj_t * create_scr_network_info(SystemModule * module) {
     ui_lbNWPool = createDefalutLabel(scr, 0xffffff,LV_TEXT_ALIGN_LEFT,&font_XinYin_reg12,72,107);
     lv_label_set_text(ui_lbNWPool,"cksolo.xaiten.com:14400");
     ui_lbNWMAC = createDefalutLabel(scr, 0xffffff,LV_TEXT_ALIGN_LEFT,&font_XinYin_reg10,212,52);
+    lv_label_set_long_mode(ui_lbNWMAC, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_width(ui_lbNWMAC,80);
     lv_label_set_text(ui_lbNWMAC,module->mac);
     ui_lbNWAddr = createDefalutLabel(scr, 0xffffff,LV_TEXT_ALIGN_LEFT,&font_XinYin_reg10,72,135);
     lv_label_set_text(ui_lbNWAddr,"afB223jjs");
+    lv_label_set_long_mode(ui_lbNWAddr, LV_LABEL_LONG_SCROLL_CIRCULAR);
+    lv_obj_set_width(ui_lbNWAddr,205);
 
     return scr;
 }
