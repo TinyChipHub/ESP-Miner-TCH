dc4d3fc954bbe755a5fe1c5c395b02474aa1ef11
AaronCai <aaron.cai@xaiten.com>
2025-08-29 10:07:08 +0800

    fix 302 TPS config issue

--- main/power/TPS546.c
+++ main/power/TPS546.c
@@ -372,7 +372,8 @@ esp_err_t TPS546_init(TPS546_CONFIG config)
     smb_write_byte(PMBUS_OPERATION, u8_value);
 
     /* Make sure power is turned off until commanded */
-    u8_value = (ON_OFF_CONFIG_DELAY | ON_OFF_CONFIG_POLARITY | ON_OFF_CONFIG_CP | ON_OFF_CONFIG_CMD | ON_OFF_CONFIG_PU);
+    u8_value = (board_version == 302) ? (ON_OFF_CONFIG_DELAY | ON_OFF_CONFIG_POLARITY | ON_OFF_CONFIG_CMD | ON_OFF_CONFIG_PU)
+        : (ON_OFF_CONFIG_DELAY | ON_OFF_CONFIG_POLARITY | ON_OFF_CONFIG_CP | ON_OFF_CONFIG_CMD | ON_OFF_CONFIG_PU);
     ESP_LOGI(TAG, "Power config-ON_OFF_CONFIG: %02X", u8_value);
     smb_write_byte(PMBUS_ON_OFF_CONFIG, u8_value);
 
